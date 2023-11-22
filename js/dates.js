c4b1a7db36b4cc0b968edf69ee358ab88874322c
waledzee <waledzaki201@gmail.com>
2023-11-22 17:48:44 +0200

    dates

---
+++ js/dates.js
@@ -0,0 +1,12 @@
+
+
+let data1= new Date("2023-01,01")
+console.log(data1);
+
+let date2=new Date();  //now
+
+console.log(date2);
+
+
+console.log(`full year is ${data1.getFullYear()}`);
+console.log(`full month is ${data1.getMonth()}`);
