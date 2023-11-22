d5ffc82b47d55911ccc0b207fe83ccf8ecbbbd86
waledzee <waledzaki201@gmail.com>
2023-11-22 15:24:35 +0200

    using operator +

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 let fristname="waled" , lastname="zaki";
 
+
+console.log(1);
+console.log(2);
+
+console.log(3);
+let sum= 1+2;
+
+console.log("sum of nums is " +sum);
+
 console.log(`hello  ${fristname}` );
