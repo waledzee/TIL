ec4a2a8157b705766f6e95c8445fc4b80aa392dd
waledzee <waledzaki201@gmail.com>
2023-11-22 17:01:49 +0200

    string -> length , quotes, uppercase,lowercas ...etx

---
+++ js/string.js
@@ -0,0 +1,35 @@
+
+
+let fristName= "Waled";
+
+let lastName= 'zaki';
+let title =`vp of Engineering`;
+
+let fullName= fristName +" "+ lastName;
+
+    
+fullName =`${fristName}+${lastName}`;
+
+//console.log(fullName);
+
+
+//string length
+
+let quote="waled abdelrhaman";
+
+console.log(quote.length);
+
+// upper case and lower case
+
+console.log(quote.toLowerCase());
+console.log(quote.toUpperCase());
+
+//  embding 
+
+let name ="zaki";
+let embding= `name is ${name}`;
+console.log(embding);   
+
+let badquote= 'a wise man once said "life is very fast and changeable"';
+
+console.log(badquote);
