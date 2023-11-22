82bde28b63985b95f9c3065295dae829285a4b48
waledzee <waledzaki201@gmail.com>
2023-11-22 15:56:08 +0200

    wrapper method

---
+++ js/primitives.js
@@ -0,0 +1,8 @@
+
+let name ="waled abdlerhaman";
+
+let upperCase = name.toUpperCase();
+
+console.log(upperCase);
+
+//  the primitives datatypes like strings has a wrapper method like toUpperCase()
