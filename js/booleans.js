c5e239b836fb7af8327239e87f54361f0c8a3ef7
waledzee <waledzaki201@gmail.com>
2023-11-22 17:19:42 +0200

    booleans data type

---
+++ js/booleans.js
@@ -0,0 +1,11 @@
+let truevalue=true;
+console.log(truevalue);
+
+let falsevalue=false;
+console.log(falsevalue);
+
+let notTrue=!true;
+console.log(notTrue);
+
+let notfalse=!false;
+console.log(notfalse);
