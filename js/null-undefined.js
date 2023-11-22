610290110e62e398938e65ca293d149375285bcc
waledzee <waledzaki201@gmail.com>
2023-11-22 17:24:49 +0200

    null -undefined data type

---
+++ js/null-undefined.js
@@ -0,0 +1,16 @@
+
+
+// what's happen when don't give a specifice value to variable
+
+let x;
+console.log(x);
+console.log(typeof(x));
+
+x="hello";
+
+let y=null;
+console.log(y);
+console.log(typeof(y));
+
+
+
