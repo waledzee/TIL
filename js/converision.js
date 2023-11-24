394f6d6cfadc468c3662740a95930657266593ce
waledzee <waledzaki201@gmail.com>
2023-11-24 16:03:21 +0200

    convertion datat types from and to

--- js/converision.js
+++ js/converision.js
@@ -12,3 +12,29 @@ console.log(typeof(stringvalue));
 
 let boolvalue=true;
 console.log(typeof(boolvalue));
+
+//conversion
+
+
+//convert from string to number
+let stringval="2";
+
+stringval= Number(stringval);
+
+console.log(typeof(stringval));
+
+// convert from string to boolen data type
+
+let str="true";
+
+str =Boolean (str);
+
+console.log(typeof(str));
+
+// convert to object using new keyword
+
+let str1 ="true";
+
+str = new Boolean (str1);
+
+console.log(typeof(str));
