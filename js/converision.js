455eeb90de35f27be49ee3ca9ed452d38e925c60
waledzee <waledzaki201@gmail.com>
2023-11-24 15:59:47 +0200

    data conversion

---
+++ js/converision.js
@@ -0,0 +1,14 @@
+
+
+let intvalue=1;
+console.log(typeof(intvalue));
+
+// string
+
+let stringvalue="Hi";
+console.log(typeof(stringvalue));
+
+//boolen
+
+let boolvalue=true;
+console.log(typeof(boolvalue));
